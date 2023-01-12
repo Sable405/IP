96216aaacbe046c6f95b610ec9477dbfd174e170
Sable405 <111007485+Sable405@users.noreply.github.com>
2023-01-12 10:54:43 -0800

    Done fix bugs

---
+++ IP man/Assets/Scripts/Scene.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void End()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
